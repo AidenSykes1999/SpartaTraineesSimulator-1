9c79163ac86f570aa1260de260b2d5b3c8d47588
Piotr Sulek <piotr.sulek@outlook.com>
2022-02-14 12:40:16 +0000

    Added Trainee and Controller

--- src/main/java/com/sparta/spartatraineesimulator/Main.java
+++ src/main/java/com/sparta/spartatraineesimulator/Main.java
@@ -1,9 +1,14 @@
 package com.sparta.spartatraineesimulator;
 
+import com.sparta.spartatraineesimulator.controller.Controller;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        Controller controller = new Controller();
+        controller.runSimulation(12);
     }
 
 }
