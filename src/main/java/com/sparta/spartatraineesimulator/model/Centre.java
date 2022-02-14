724a7cde02b2f7f119d085a893ef6c65e354bb4b
Roberto Lovece <rlovece@hotmail.co.uk>
2022-02-14 12:38:53 +0000

    Added basics to centre

--- src/main/java/com/sparta/spartatraineesimulator/model/Centre.java
+++ src/main/java/com/sparta/spartatraineesimulator/model/Centre.java
@@ -5,5 +5,19 @@ import java.util.ArrayList;
 public class Centre {
 
     private ArrayList<Trainee> trainees;
+    private static final int LIMIT = 100;
+    private int currentCapacity = 0;
+
+    public Centre(ArrayList<Trainee> trainees) {
+        this.trainees = trainees;
+    }
+
+    public ArrayList<Trainee> getTrainees() {
+        return trainees;
+    }
+
+    public void setTrainees(ArrayList<Trainee> trainees) {
+        this.trainees = trainees;
+    }
 
 }
