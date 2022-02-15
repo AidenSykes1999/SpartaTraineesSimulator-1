42dd3d66212bf4651ba1feb363d92a274f9ec00d
suyash2016s <suyash2016s@gmail.com>
2022-02-15 15:07:16 +0000

    Phase 2- Created Enum class for the courses and a ProjectRandomGenerator class for chosing a random course for a trainee.
    Phase 2- Created Enum class for the courses, and a ProjectRandomGenerator class for choosing a random course (safe) for a trainee.
    
    In Trainee class- Created boolean isWaiting variable (can be used for the waiting list), assigned a random course method, also created get/set methods.
    
    Tests for ProjectRandomGenerator and Courses- In process.

--- src/main/java/com/sparta/spartatraineesimulator/model/Trainee.java
+++ src/main/java/com/sparta/spartatraineesimulator/model/Trainee.java
@@ -4,6 +4,10 @@ public class Trainee {
     private int traineeId;
     private int trainingTime = 0;
 
+    // new variables for the waiting list
+    private boolean isWaiting = true;
+    private final Courses courseType = Courses.randomCourseType(); //random course for a trainee
+
     public Trainee(int traineeId, int trainingTime){
         this.traineeId = traineeId;
         this.trainingTime = trainingTime;
@@ -30,5 +34,20 @@ public class Trainee {
         return "Trainee{" +
                 "traineeId=" + traineeId +
                 '}';
+    }
+
+    //setter for the waiting
+    public void setIsWaiting(boolean isWaiting) {
+        this.isWaiting = isWaiting;
+    }
+
+    //getter for the waiting
+    public boolean isWaiting() {
+        return isWaiting;
+    }
+
+    //getter for the trainee's course type
+    public Courses getCourseType() {
+        return courseType;
     }
 }
