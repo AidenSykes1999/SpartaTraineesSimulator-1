7e839f14063fb7b7f1d1993b48e8868ab9982963
Roberto Lovece <rlovece@hotmail.co.uk>
2022-02-15 11:32:35 +0000

    started refactoring controller

--- src/main/java/com/sparta/spartatraineesimulator/Main.java
+++ src/main/java/com/sparta/spartatraineesimulator/Main.java
@@ -5,12 +5,29 @@ import com.sparta.spartatraineesimulator.view.DisplayManager;
 
 public class Main {
 
+    private static int currentMonth;
+
     public static void main(String[] args) {
         System.out.println("Hello");
         DisplayManager dm = new DisplayManager();
         Controller controller = new Controller();
+
         int months = dm.numberOfMonths();
-        controller.runSimulation(months);
+
+        for(int i = 0; i < months; i++) {
+
+            controller.runSimulationTick(i);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            currentMonth++;
+
+        }
+
     }
 
 }
--- src/main/java/com/sparta/spartatraineesimulator/controller/Controller.java
+++ src/main/java/com/sparta/spartatraineesimulator/controller/Controller.java
@@ -14,81 +14,54 @@ public class Controller {
 
     private int totalEnlisted = 0;
     private int traineeId = 0;
-    private int totalTraining = 0;
+
     private ArrayList<Centre> centres = new ArrayList<>();
     private ArrayList<Trainee> waitingList = new ArrayList<>();
 
-    public void runSimulation(int month) {
-        DisplayManager dm = new DisplayManager();
-        for(int i = 0; i < month; i++){
+    public void runSimulationTick (int month) {
 
-            ArrayList<Trainee> newTrainees = generateTrainees();
-            waitingList.addAll(newTrainees);
-            totalEnlisted += newTrainees.size();
+        ArrayList<Trainee> newTrainees = generateTrainees();
+        waitingList.addAll(newTrainees);
+        totalEnlisted += newTrainees.size();
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        System.out.println("1 month passed");
+        createCenter(month);
+        addTraineesToCentres();
 
-            System.out.println("1 month passed");
-            if(i % 2 == 0){
-                centres.add(new Centre());
-            }
-
-            for (Centre centre : centres) {
-                if (!centre.isCentreFull()) {
-                    int freeSpace = centre.getEmptySpace();
-
-                    if (freeSpace >= waitingList.size()) {
-                        centre.addAllTrainees(waitingList);
-                        waitingList.clear();
-                    }
-                    else if (freeSpace < waitingList.size()) {
-                        List<Trainee> subListTrainee = waitingList.subList(0, freeSpace);
-                        centre.addAllTrainees(subListTrainee);
-                        waitingList.removeAll(subListTrainee);
-                    }
-
-                }
-            }
-
-            for (Centre centre : centres) {
-                System.out.print(centre.getCurrentCapacity() + ", ");
-            }
-            System.out.println("Waiting list size: " + waitingList.size());
-
-           // for(int j = 0; j < trainees.size(); i++){
-            //     while(trainees.get(j).getTrainingTime() < )
-            //     trainees.get(j).incrementTrainingTime();
-            // }
+        for (Centre centre : centres) {
+            System.out.print(centre.getCurrentCapacity() + ", ");
         }
+        System.out.println("Waiting list size: " + waitingList.size());
+
+
 
         System.out.println("Total enlisted: " + totalEnlisted);
 
         int totalFullCenters = 0;
+        int totalTraining = 0;
+
         for (Centre centre : centres) {
-            totalFullCenters += centre.getCurrentCapacity()/100;
+
+            if (centre.isCentreFull()) {
+                totalFullCenters++;
+            }
+
+            totalTraining += centre.getCurrentCapacity();
         }
-        if (totalEnlisted > (centres.size()*100)){
-            totalTraining = centres.size()*100;
-        }
-        else
-            totalTraining = totalEnlisted;
+
 
         System.out.println(dm.displayTheDetails(centres.size(), totalFullCenters, totalTraining, waitingList.size()));
+//      System.out.println("Total centres: " + totalCenters/100);
+//      System.out.println("Total waitingList size: " + waitingList.size());
 
-//        System.out.println("Total centres: " + totalCenters/100);
-//        System.out.println("Total waitingList size: " + waitingList.size());
     }
 
-    public ArrayList<Trainee> generateTrainees(){
+    public ArrayList<Trainee> generateTrainees () {
         Random r = new Random();
         int numberOfTrainees = r.nextInt(50, 101);
         ArrayList<Trainee> newTrainees = new ArrayList<Trainee>();
 
-        for(int i = 0; i < numberOfTrainees; i++){
+        for (int i = 0; i < numberOfTrainees; i++) {
             newTrainees.add(new Trainee(traineeId, 0));
             traineeId++;
         }
@@ -96,15 +69,41 @@ public class Controller {
         return newTrainees;
     }
 
-    public int getTotalEnlisted() {
+    private void createCenter (int month){
+        if (month % 2 == 0) {
+            centres.add(new Centre());
+        }
+    }
+
+    private void addTraineesToCentres () {
+        for (Centre centre : centres) {
+            if (!centre.isCentreFull()) {
+                int freeSpace = centre.getEmptySpace();
+
+                if (freeSpace >= waitingList.size()) {
+                    centre.addAllTrainees(waitingList);
+                    waitingList.clear();
+                } else if (freeSpace < waitingList.size()) {
+                    List<Trainee> subListTrainee = waitingList.subList(0, freeSpace);
+                    centre.addAllTrainees(subListTrainee);
+                    waitingList.removeAll(subListTrainee);
+                }
+            }
+        }
+    }
+
+
+    public int getTotalEnlisted () {
         return totalEnlisted;
     }
 
-    public ArrayList<Centre> getCentres() {
+    public ArrayList<Centre> getCentres () {
         return centres;
     }
 
-    public ArrayList<Trainee> getWaitingList() {
+    public ArrayList<Trainee> getWaitingList () {
         return waitingList;
     }
+
 }
+
